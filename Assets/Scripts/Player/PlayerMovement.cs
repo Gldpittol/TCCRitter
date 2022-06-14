88c2da8167ed31a931939d5c6f8e3db3828e6b2b
Gldpittol <gabrielpittol@hotmail.com>
2022-06-13 21:04:52 -0300

    Dash, Small Fixes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class PlayerMovement : MonoBehaviour
     private PlayerAnimations _playerAnimations;
     private Rigidbody2D _rigidBody;
     private AudioSource _audSource;
+    public float dashDelay = 2f;
+    public float dashForce = 10f;
+
+    private float currentDashDelay = 0;
+    private bool lastRight = false;
     private void Awake()
     {
         if (PlayerDontDestroy.Instance && PlayerDontDestroy.Instance.gameObject != transform.parent.gameObject) return;
@@ -31,16 +37,43 @@ public class PlayerMovement : MonoBehaviour
         if(!FKey) FKey = PlayerManager.Instance.exclamationMark.GetComponent<SpriteRenderer>();
         FKey.enabled = active;
     }
-    
+
+    private void Update()
+    {
+        currentDashDelay -= Time.deltaTime;
+    }
+
 
     private void FixedUpdate()
     {
-        if (GameManager.Instance.gameState == GameState.Gameplay) MoveCharacter();
+        if (GameManager.Instance.gameState == GameState.Gameplay)
+        {
+            MoveCharacter();
+            CheckDash();
+        }
         else
         {
             _rigidBody.velocity = new Vector2(0, 0);
             if(_playerAnimations) _playerAnimations.SetIdle();
             _audSource.enabled = false;
+        }
+    }
+
+    private void CheckDash()
+    {
+        if (currentDashDelay > 0) return;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 impulseVector = new Vector2(Input.GetAxisRaw("Horizontal"),
+                Input.GetAxisRaw("Vertical")).normalized;
+            if (impulseVector == Vector2.zero)
+            {
+                if (lastRight) impulseVector = new Vector2(1, 0);
+                else impulseVector = new Vector2(-1, 0);
+            }
+            
+            _rigidBody.AddForce(impulseVector * dashForce, ForceMode2D.Impulse);
+            currentDashDelay = dashDelay;
         }
     }
 
@@ -55,6 +88,9 @@ public class PlayerMovement : MonoBehaviour
         _rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 
             Input.GetAxisRaw("Vertical")).normalized * speed * PlayerStats.movementSpeedMultiplier;
 
+        if (Input.GetAxisRaw("Horizontal") > 0) lastRight = true;
+        else if (Input.GetAxisRaw("Horizontal") < 0) lastRight = false;
+        
         if (_rigidBody.velocity != Vector2.zero && !_audSource.enabled) _audSource.enabled = true;
         if (_rigidBody.velocity == Vector2.zero && _audSource.enabled) _audSource.enabled = false;
     }
